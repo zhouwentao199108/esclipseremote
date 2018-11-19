185d3a19ffe9d8ffad7fd8d84cce61d1de5c6c83
yq <246375818@qq.com>
2018-11-19 18:45:45 +0800

    合并冲突

--- HelloWorld/src/main/java/org/lanqiao/maven/HelloWorld.java
+++ HelloWorld/src/main/java/org/lanqiao/maven/HelloWorld.java
@@ -1,9 +1,5 @@
 package org.lanqiao.maven;
-<<<<<<< HEAD
 //aaa
-=======
-//bbb
->>>>>>> branch 'HEAD' of https://github.com/zhouwentao199108/esclipseremote.git
 public class HelloWorld{
 	public String sayHello(String name){
 		return "Hello "+name ;
